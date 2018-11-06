2fcaf93485aa5db3492be5016297a7b91cf179d9
IvanSitnikov <sit7602@gmail.com>
2018-11-06 22:04:16 +0500

    add json to xml example

--- sdek_api.py
+++ sdek_api.py
@@ -4,11 +4,22 @@ from time import sleep
 import xmltodict
 import json
 
-
 #  Request description
 req = requests.get('https://integration.cdek.ru/pvzlist/v1/xml')
 req = req.text
+# XML to JSON
 data = xmltodict.parse(req)
 data = json.dumps(data)
 data = json.loads(data)
 print(data['PvzList']['Pvz'][0])
+
+# JSON to XML
+test_response = {
+    'key1':
+    {
+        'key11':'value1',
+        'key12':'value2'
+    }
+}
+test_response = xmltodict.unparse(test_response, pretty=True)
+print(test_response)
