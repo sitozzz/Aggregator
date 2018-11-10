e2188f7912276f2c7eec136c1bae7fcbe52e3855
Ivan Notebook <sit7602@gmail.com>
2018-11-10 13:38:10 +0300

    add test files for sdek

--- sdek_api.py
+++ sdek_api.py
@@ -1,25 +1,63 @@
 import requests
-from xml.dom import minidom
 from time import sleep
 import xmltodict
 import json
+import hashlib
+test_login = "3y03Ahh2XigMgzN1l7mWff6jZMSCqHE1"
+dateExecute = "2018-12-12"
+test_pswd = dateExecute + "&Or6kB0BaKUrGCx51NWtVNcZ4YmVRFMBN"
 
-#  Request description
-req = requests.get('https://integration.cdek.ru/pvzlist/v1/xml')
-req = req.text
-# XML to JSON
-data = xmltodict.parse(req)
-data = json.dumps(data)
-data = json.loads(data)
-print(data['PvzList']['Pvz'][0])
+req = { 
+	"version":"1.0",
+	"dateExecute":"2012-07-27", 
+	"authLogin":test_login, 
+	"secure": hashlib.md5(test_pswd.encode('utf-8')).hexdigest(), 
+	"senderCityId":"270", 
+	"receiverCityId":"44", 
+	"tariffId":"137", 
+	"goods": 
+		[ 
+			{ 
+				"weight":"0.3", 
+				"length":"10", 
+				"width":"7", 
+				"height":"5" 
+			}, 
+			{ 
+				"weight":"0.1", 
+				"volume":"0.1" 
+			} 
+		],
+	"services": [
+		{	
+			"id": 2,	
+			"param": 2000	
+		},
+		{	
+			"id": 30
+		}
+	]
+} 	
 
-# JSON to XML
-test_response = {
-    'key1':
-    {
-        'key11':'value1',
-        'key12':'value2'
-    }
-}
-test_response = xmltodict.unparse(test_response, pretty=True)
-print(test_response)
+# result = requests.get
+
+
+# #  Request description
+# req = requests.get('https://integration.cdek.ru/pvzlist/v1/xml')
+# req = req.text
+# # XML to JSON
+# data = xmltodict.parse(req)
+# data = json.dumps(data)
+# data = json.loads(data)
+# print(data['PvzList']['Pvz'][0])
+
+# # JSON to XML
+# test_response = {
+#     'key1':
+#     {
+#         'key11':'value1',
+#         'key12':'value2'
+#     }
+# }
+# test_response = xmltodict.unparse(test_response, pretty=True)
+# print(test_response)
